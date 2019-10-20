c3442c9f06ac9b60f8caeab7d43c8f8f53539faa
Ryan Ku <ryanku98@gmail.com>
2019-10-19 22:41:46 -0700

    Posts are now individual tweets (as opposed to replies)

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import tweepy   # Twitter API Python wrapper
 import os       # path.exists
 import requests # get
 import time     # sleep
-# import json
 
 a_key_path = "config/access_key.txt"
 a_sec_path = "config/access_secret.txt"
@@ -13,7 +12,9 @@ image_file_path = "image."
 deepai_api_key_path = "config/deepai_api_key.txt"
 deepdream_api_url = "https://api.deepai.org/api/deepdream"
 microsoft_image_description_api_url = "https://microsoft-azure-microsoft-computer-vision-v1.p.rapidapi.com/describe"
+my_username = "deepdreamrepost"
 tweet_count = 0
+error_count = 0
 api_retries = 3
 
 def repost():
@@ -31,7 +32,7 @@ def repost():
     c_key_file.close()
     c_sec_file.close()
     
-    # authentication
+    # Twitter authentication
     auth = tweepy.OAuthHandler(c_key, c_sec)
     auth.set_access_token(a_key, a_sec)
     api = tweepy.API(auth)
@@ -47,47 +48,54 @@ def repost():
         my_mentions = api.mentions_timeline(last_tweet_id, tweet_mode="extended")
     else:
         my_mentions = api.mentions_timeline(tweet_mode="extended")
-        
-    # print(my_mentions[0].id)
     
-    # for mention in my_mentions:
-    #     print(json.dumps(mention._json))
-    
+    global tweet_count, error_count
     for mention in reversed(my_mentions):
-        print("Found tweet id: " + str(mention.id))
+        print("Found tweet " + get_tweet_url(mention.user.screen_name, mention.id))
         try:
+            # tweets without media don't have the extended_entities attribute
             media_type = mention.extended_entities["media"][0]["type"]
         except:
             print("No media found")
             api.update_status(status="No media found - tweet me an image to see the Deep Dream generated version! If you believe this is an error, feel free to open an issue ____ and link your tweet!", in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
             set_last_tweet(mention.id)
+            # update error counter
+            error_count += 1
+            # go to next mention
             continue
+
         if media_type == "video":
             print("Videos not supported")
             api.update_status(status="Videos not supported - tweet me an image instead! If you believe this is an error, feel free to open an issue ____ and link your tweet!", in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
             set_last_tweet(mention.id)
+            # update error counter
+            error_count += 1
         elif media_type == "photo":
             media_url = mention.extended_entities["media"][0]["media_url_https"]
-            print(media_url)
             get_hashtags(mention.entities["hashtags"], media_url)
             filename = deepdream(media_url)
-            api.update_with_media(filename, status=get_hashtags(mention.entities["hashtags"], media_url), in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
+            # tweet deepdream photo
+            t = api.update_with_media(filename, status="#deepdream " + get_hashtags(mention.entities["hashtags"], media_url))
+            print("Generated " + get_tweet_url(my_username, t.id))
+            # reply to original tweet with deepdream tweet
+            api.update_status(status=get_tweet_url(my_username, t.id), in_reply_to_status_id=mention.id, auto_populate_reply_metadata=True)
             set_last_tweet(mention.id)
-            global tweet_count
+            # update counter
             tweet_count += 1
         
     delete_media()
-    print("Replied to " + str(tweet_count) + " tweets!")
+    print_counters()
     return
 
 # send image through deepdream API, return name of file in local storage
 def deepdream(media_url):
-    # generate deepdream style
+    # set deepai metadata
     data = {"image": media_url}
     deepai_api_key_file = open(deepai_api_key_path, "r")
     headers = {"api-key": deepai_api_key_file.read()}
     deepai_api_key_file.close()
     
+    # try deepai API
     for x in range(api_retries):
         r = requests.post(deepdream_api_url, data=data, headers=headers)
         if r.status_code != 200:
@@ -109,6 +117,9 @@ def deepdream(media_url):
     
     return filename
     
+def get_tweet_url(username, id):
+    return "https://twitter.com/" + username + "/status/" + str(id)
+    
 def set_last_tweet(last_id):
     last_tweet_id_file = open(last_tweet_id_path, "w")
     last_tweet_id_file.write(str(last_id))
@@ -122,10 +133,10 @@ def get_image_type(link_str):
     
 def get_hashtags(hash_dict, media_url):
     # first get original poster hashtags
-    hash_set = {"deepdream"}
+    hash_set = set()
     for tag in hash_dict:
         hash_set.add(tag["text"])
-    # use image recognition API
+    # set rapidapi metadata
     data = "{\"url\":\"" + media_url + "\"}"
     x_rapidapi_key_file = open("config/x_rapidapi_key.txt")
     headers = {
@@ -135,7 +146,7 @@ def get_hashtags(hash_dict, media_url):
         "accept": "application/json"
     }
     x_rapidapi_key_file.close()
-    
+    # try rapidapi image recognition API
     for x in range(api_retries):
         r = requests.post(microsoft_image_description_api_url, data=data, headers=headers)
         if r.status_code != 200:
@@ -159,6 +170,9 @@ def get_hashtags(hash_dict, media_url):
 def delete_media():
     if os.path.exists(image_file_path):
         os.remove(image_file_path)
+        
+def print_counters():
+    print(str(tweet_count) + " tweets | " + str(error_count) + " errors")
     
 if __name__ == "__main__":
     repost()
