2a92309f6ab71d2439dfa8cfc75414bedf3b6a96
Ryan Ku <ryanku98@gmail.com>
2019-10-22 22:29:19 -0700

    Initial commit

---
+++ clear_timeline.py
@@ -0,0 +1,23 @@
+import tweepy
+from bot import authorize_twitter, my_username
+
+def wipe_timeline():
+    """Simple function for wiping everything from a timeline (useful when dev testing)"""
+    api = authorize_twitter()
+    
+    count = 0
+    while True:
+        my_timeline = api.user_timeline(my_username)
+        
+        if len(my_timeline) == 0:
+            break   # exit if timeline is empty
+        
+        for tweet in my_timeline:
+            api.destroy_status(tweet.id)
+            count += 1
+    
+    print(str(count) + " tweets deleted!")
+    return
+    
+if __name__ == "__main__":
+    wipe_timeline()
