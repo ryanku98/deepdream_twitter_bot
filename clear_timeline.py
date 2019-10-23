f08c56401e0e069e509e1d41e866c39251a26a30
Ryan Ku <ryanku98@gmail.com>
2019-10-22 22:32:08 -0700

    Resets last_tweet_id

--- clear_timeline.py
+++ clear_timeline.py
@@ -1,5 +1,5 @@
 import tweepy
-from bot import authorize_twitter, my_username
+from bot import authorize_twitter, set_last_tweet_id, my_username
 
 def wipe_timeline():
     """Simple function for wiping everything from a timeline (useful when dev testing)"""
@@ -14,7 +14,11 @@ def wipe_timeline():
         
         for tweet in my_timeline:
             api.destroy_status(tweet.id)
+            print("Deleted tweet ID: " + str(tweet.id))
             count += 1
+            
+    # reset last_tweet_id
+    set_last_tweet_id(1)
     
     print(str(count) + " tweets deleted!")
     return
